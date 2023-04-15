98d14ad5a3cc8d9fd564c3a76c6395c8321d3e7f
Kiisi <kiisifelix06@gmail.com>
2023-04-15 12:51:52 +0100

    Added Userinfo route

--- controllers/profileController.js
+++ controllers/profileController.js
@@ -1,5 +1,6 @@
 const UserModel = require("../models/userModel")
 
+// User profile
 module.exports.profile = async (req, res) =>{
     const id = req.params.id
     console.log(id)
@@ -28,16 +29,61 @@ module.exports.profile = async (req, res) =>{
     const { height, weight, blood_group, genotype} = req.body
 
     try{
-        let u = await UserModel.updateOne({ _id: id }, { 
+        await UserModel.updateOne({ _id: id }, { 
             height: height, 
             weight: weight,
             blood_group: blood_group,
             genotype: genotype 
         });
-        console.log(u)
         res.status(202).json({success: 'Update successful'})
     }
     catch(err){
+        res.status(401).json({success: 'An error occurred'})
+        console.log(err)
+    }
+
+}
+
+
+// User Info
+module.exports.userInfo = async (req, res) =>{
+    const id = req.params.id
+    console.log(id)
+    const user_info_data = req.body
+
+    let errors = {
+        'state':'',
+        'city':'',
+        'allergies':'',
+        'description':'',
+    }
+
+    let error_bool = false
+
+    for (let key in user_info_data){
+        if(user_info_data[key].trim() === ''){
+            errors[key] = 'This is field is empty!'
+            error_bool = true
+        }
+    }
+
+    if(error_bool){
+        return res.status(403).json({error: errors})
+    }
+
+    const { state, city, allergies, description } = req.body
+
+    try{
+        await UserModel.updateOne({ _id: id }, { 
+            state: state, 
+            city: city,
+            allergies: allergies,
+            description: description 
+        });
+        res.status(202).json({success: 'Update successful'})
+    }
+    catch(err){
+        res.status(401).json({success: 'An error occurred'})
         console.log(err)
     }
 
--- routes/profileRoute.js
+++ routes/profileRoute.js
@@ -4,4 +4,7 @@ const profileController = require('../controllers/profileController')
 // Profile Route
 router.post('/profile/:id', profileController.profile)
 
+// User Info Route
+router.post('/user-info/:id', profileController.userInfo)
+
 module.exports = router
